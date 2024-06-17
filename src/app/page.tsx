f0ec04bae85f0c390b8e706f5c6b85138226650b
Lakavath Ashish Sai Naik <96200707+ashishsai960@users.noreply.github.com>
2024-06-17 17:12:13 +0530

    update the jobs names from the books and the i have only mentioned one gateway method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,18 @@ import RazorpayForm from "../components/RazorpayForm";
 import styles from './page.module.css';
 
 export default function Home() {
-  const [book, setBook] = useState({
-    name: "Razorpay and Stripe Integration",
-    author: "Ritvik",
+  const [job, setjob] = useState({
+    name: "Razorpay Integration",
+    author: "Ashish sai naik",
     img: "https://akaunting.com/public/assets/media/54-mark-britto/razorpay/razorpay-logo.jpg",
     price: 250,
   });
 
   return (
     <div className={styles.app}>
-      <div className={styles.bookContainer}>
-        <img src={book.img} alt="book_img" className={styles.bookImg} />
-        <p className={styles.bookName}>{book.name}</p>
-        <p className={styles.bookAuthor}>By {book.author}</p>
-        <p className={styles.bookPrice}>
-          Price : <span>&#x20B9; {book.price}</span>
-        </p>
-        <Elements stripe={loadStripe("pk_test_51PMUE8JQvHmO1Z0X7o4R41cFYGKscBU3nBV8FyxF5HAbT5swoRLO4MsIgC7YLtGHnxcegOLTkt1O8Nt3TAbyFvH600uUhbyql1")}>
-          {/* enter your publishable key  inside loadStripe("your publishable key")*/}
-          <CheckoutForm book={book} />
-        </Elements>
-        <RazorpayForm book={book} />
+      
+        <RazorpayForm job={job} />
       </div>
-    </div>
+    
   )
 };
--- src/components/RazorpayForm.tsx
+++ src/components/RazorpayForm.tsx
@@ -4,25 +4,25 @@ import { useState } from "react";
 import axios from "axios";
 import styles from './RazorpayForm.module.css';
 
-const RazorpayForm = ({ book }) => {
+const RazorpayForm = ({ job }) => {
     const [error, setError] = useState(null);
 
     const handleRazorpayPayment = async () => {
         try {
-            const orderUrl = "http://44.211.47.208:8080/api/payment/orders";
+            const orderUrl = "http://51.20.106.102:9000/api/payment/orders";
             //replace the orderurl with your server endpoint
-            const { data } = await axios.post(orderUrl, { amount: book.price, userId: "123456" });
+            const { data } = await axios.post(orderUrl, { amount: job.price, userId: "123456" });
             const options = {
                 key: "rzp_test_0pLj1oTm2LU4c9", // Your Razorpay keyid
                 amount: data.data.amount,
                 currency: data.data.currency,
-                name: book.name,
+                name: job.name,
                 description: "Test Transaction",
-                image: book.img,
+                image: job.img,
                 order_id: data.data.id,
                 handler: async (response) => {
                     try {
-                        const verifyUrl = "http://44.211.47.208:8080/api/payment/verify";
+                        const verifyUrl = "http://51.20.106.102:9000/api/payment/verify";
                         //replace the url with your server endpoint
                         const { data } = await axios.post(verifyUrl, response);
                         console.log(data);
@@ -41,12 +41,18 @@ const RazorpayForm = ({ book }) => {
             setError("Error processing payment with Razorpay");
         }
     };
+    const [jobs, setjob] = useState({
+        name: "Razorpay Integration",
+        author: "Ashish sai naik",
+        img: "https://akaunting.com/public/assets/media/54-mark-britto/razorpay/razorpay-logo.jpg",
+        price: 250,
+      });
 
     return (
         <div>
             {error && <div className={styles.error}>{error}</div>}
-            <button onClick={handleRazorpayPayment} disabled={book.price <= 0} className={styles.button}>
-                Pay with Razorpay
+            <button onClick={handleRazorpayPayment} disabled={jobs.price <= 0} className={styles.button}>
+                Pay <span>&#x20B9; {jobs.price}</span> with Razorpay
             </button>
         </div>
     );
