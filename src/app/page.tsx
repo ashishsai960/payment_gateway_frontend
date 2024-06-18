54e514bcd876d3e99e86d3ca0c70dd955efce049
Lakavath Ashish Sai Naik <96200707+ashishsai960@users.noreply.github.com>
2024-06-18 16:34:07 +0530

    removed the checkout form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from "../components/CheckoutForm";
+
 import RazorpayForm from "../components/RazorpayForm";
 import styles from './page.module.css';
 
